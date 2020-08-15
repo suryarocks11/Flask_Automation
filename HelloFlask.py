c352bbfec60b2cd30760fbe4f8b988e8d8b5af72
Surya Prakash Gupta <sugamprakash@gmail.com>
2020-08-15 15:03:53 +0530

    Flask

--- HelloFlask.py
+++ HelloFlask.py
@@ -1,2 +1,10 @@
-print ('Hello World')
-print ('Hello World')
+from flask import Flask
+app =Flask(__name__)
+
+@app.route("/")
+def home():
+    return("Hello this is Flask")
+
+    if __name__ == "__main__":
+        app.run()
+print('Running')
